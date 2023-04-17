97fdcdd652d8fee1822ad7c28f3fc7b2ae5b47c3
Igor Pujolm <igor.pujolpp@gmail.com>
2023-04-17 10:57:16 -0300

    Refactored GenerateIntList function to return an ordered list. Fixed a bug in BinarySearch function's while loop to correctly handle the middle index. Also, addressed an issue with the comparison between the guess and target variables for accurate search results.

--- main.py
+++ main.py
@@ -20,34 +20,35 @@ def BinarySearch(input_list: list[int], target: int) -> int:
     # Local variables
     left_index = 0
     right_index = len(input_list) - 1
-
+    print(input_list)
     # Function loop
     while left_index <= right_index:
         middle_index = (left_index + right_index) // 2
         guess = input_list[middle_index]
-        print(f'left_index is {left_index} and right_index is {right_index}. middle_index is {middle_index}, guess is {guess}. TARGET:{target}')
+        print(f'Guess: {guess} | Target: {target} | Left: {left_index} | Right: {right_index} | Middle: {middle_index}\n')
         if guess == target:
             print(f'Target found at index: {middle_index}')
-        if guess > target:
+            break
+        if guess < target:
             left_index = middle_index + 1
         else:
             right_index = middle_index - 1
     return None
 
-def GenerateIntList(x: int, y: int) -> list[int]:
-    return random.sample(range(1, y+1), x)
+def GenerateSortedIntList(x: int, y: int) -> list[int]:
+    lst = random.sample(range(1, y+1), x)
+    lst.sort()
+    return lst
 
 def main() -> None:
     start_time = time.time()
-    int_list = GenerateIntList(6, 100)
+    int_list = GenerateSortedIntList(50, 100)
     end_time = time.time()
     print(f'The list creation took {end_time - start_time} seconds')
     start_time = time.time()
-    print(f'{int_list[3]}\n{int_list}') # temp
-    BinarySearch(int_list, int_list[3])
+    BinarySearch(int_list, 30)
     end_time = time.time()
     print(f'The search took {end_time - start_time} seconds')
-    print(f'{int_list[3]}\n{int_list}') # temp
 
 if __name__ == '__main__':
     main()
